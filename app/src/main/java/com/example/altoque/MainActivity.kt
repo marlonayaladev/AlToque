5246aa5598ef4f48579d23a8ebf98a5d30e090eb
Alan Puma Quispe <alan.puma@autonoma.pe>
2025-10-13 18:45:51 -0500

    ME QUEDE EN AÑADIRLO A NIVEL DE PROYECTO

--- app/src/main/java/com/example/altoque/MainActivity.kt
+++ app/src/main/java/com/example/altoque/MainActivity.kt
@@ -4,42 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.altoque.ui.theme.AlToqueTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        // Llama a installSplashScreen() ANTES de super.onCreate()
-        installSplashScreen()
-
         super.onCreate(savedInstanceState)
-
-        // El resto de tu código no cambia
+        enableEdgeToEdge()
         setContent {
-            // ... tu UI de Compose
+            AlToqueTheme {
+                HomeScreen()
+            }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AlToqueTheme {
-        Greeting("Android")
     }
 }
